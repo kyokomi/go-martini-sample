d858a12c620d2baac64b92462f23225e376debbe
kyokomi <kyoko1220adword@gmail.com>
2014-07-06 14:12:35 +0900

    sqlite試してみた

--- server.go
+++ server.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"github.com/go-martini/martini"
-	"code.google.com/p/leveldb-go/leveldb"
 	"log"
-	"code.google.com/p/leveldb-go/leveldb/db"
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	"fmt"
 )
 
 func main() {
 	m := martini.Classic()
 
-	var opts db.Options
-	level, err := leveldb.Open("hogeDB", &opts)
+	db, err := sql.Open("sqlite3", "./foo.db")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	defer level.Close()
+	defer db.Close()
 
-	m.Map(level)
+	sql := `
+	create table IF NOT EXISTS foo (id integer not null primary key, name text);
+	`
+	_, err = db.Exec(sql)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sql)
+		return
+	}
+
+	m.Map(db)
 	m.Get("/", func() string {
 		return "Hello world!"
 	})
@@ -28,30 +37,47 @@ func main() {
 	m.Run()
 }
 
-func RegisterData(params martini.Params, l *log.Logger, level *leveldb.DB) (int, string) {
+func RegisterData(params martini.Params, l *log.Logger, db *sql.DB) (int, string) {
 	key := params["key"]
 	value := params["value"]
 	l.Println(key, value)
 
-	var opts db.WriteOptions
-	err := level.Set([]byte(key), []byte(value), &opts)
+	tx, err := db.Begin()
 	if err != nil {
-		message := "Error! " + err.Error() + " key = " + key + " value = " + value
-		l.Fatal(message)
-		return 400, message
+		log.Fatal(err)
 	}
+	stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	for i := 0; i < 10; i++ {
+		_, err = stmt.Exec(i, fmt.Sprintf("[%s = %s] ねぷねぷのぷりん%03d", key, value, i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	tx.Commit()
+
 	return 200, "Hello!" + value
 }
 
-func ReadData(params martini.Params, l *log.Logger, level *leveldb.DB) (int, string) {
+func ReadData(params martini.Params, l *log.Logger, db *sql.DB) (int, string) {
 	key := params["key"]
-	var opts db.ReadOptions
-	value, err := level.Get([]byte(key), &opts)
-	if err != nil {
-		message := "Error! " + err.Error() + " key = " + key
-		l.Fatal(message)
-		return 400, message
-	}
 
-	return 200, "Data key = " + key + " value = " + string(value)
+	stmt, err := db.Prepare("select name from foo where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	var name string
+	err = stmt.QueryRow(key).Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name)
+
+	return 200, "Data key = " + key + " name = " + name
 }
